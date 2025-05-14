34f6c38fdf8ae8336aad270b2a930f2d78edcbca
imKrisK <imkrisk@icloud.com>
2025-05-13 21:35:12 -0700

    Refactor BitcoinRates component to use useBitcoinPriceWithReducer hook and improve currency selection UI

--- src/components/FormContainer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const FormContainer = ({ children, style }) => {
-  return <div style={style.container}>{children}</div>;
-};
-
-export default FormContainer;
--- src/pages/Homepage.jsx
+++
@@ -1,8 +0,0 @@
-export default function Homepage() { // SAve in pages/Homepage.jsx
-    return (
-        <div className="Homepage">
-            <h1>Hello World!</h1>
-        </div>
-    )
-
-}
