e90b727c6351a9e689d322fddf48cd2725f71d72
imKrisK <imkrisk@icloud.com>
2025-08-12 13:15:14 -0700

    Resolving most common cross-browser and deployment image issues

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
           <Router>
             <header className="head redesigned-header">
               <div className="logo-title redesigned-logo-title">
-                <img src="/src/assets/LFTlogo.png" alt="Food Truck Logo" className="logo-img redesigned-logo-img" />
+                <img src="/LFTlogo.png" alt="Food Truck Logo" className="logo-img redesigned-logo-img" />
                 <h1 className="site-title">Local Food <span className="brand-highlight"><i className="fas fa-truck"></i></span> Truck</h1>
               </div>
               <NavBar />
